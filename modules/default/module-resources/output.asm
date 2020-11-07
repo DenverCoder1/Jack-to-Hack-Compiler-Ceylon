c8f388febb82f584e1ef424b317b4989121200d5
DenverCoder1 <jonah@freshidea.com>
2020-11-07 20:39:15 +0200

    Added resources for lab 8

--- modules/default/module-resources/output.asm
+++ modules/default/module-resources/output.asm
@@ -28,69 +28,119 @@ D=A
 @THAT
 M=D
 
-// Translation of StaticTest.vm
+// Translation of PointerTest.vm
 // ----------------------------------
 
-// push constant 111
-@111
+// push constant 3030
+@3030
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
 
-// push constant 333
-@333
+// pop pointer 0
+@SP
+AM=M-1
+D=M
+M=0
+@3
+M=D
+
+// push constant 3040
+@3040
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
 
-// push constant 888
-@888
+// pop pointer 1
+@SP
+AM=M-1
+D=M
+M=0
+@4
+M=D
+
+// push constant 32
+@32
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
 
-// pop static 8
+// pop this 2
+@2
+D=A
+@THIS
+D=D+M
+@R13
+M=D
 @SP
 AM=M-1
 D=M
 M=0
-@StaticTest.8
+@R13
+A=M
 M=D
 
-// pop static 3
+// push constant 46
+@46
+D=A
 @SP
-AM=M-1
-D=M
-M=0
-@StaticTest.3
-M=D
-
-// pop static 1
-@SP
-AM=M-1
-D=M
-M=0
-@StaticTest.1
-M=D
-
-// push static 3
-@StaticTest.3
-D=M
-@SP@SP
 M=M+1
 A=M-1
 M=D
 
-// push static 1
-@StaticTest.1
+// pop that 6
+@6
+D=A
+@THAT
+D=D+M
+@R13
+M=D
+@SP
+AM=M-1
 D=M
-@SP@SP
+M=0
+@R13
+A=M
+M=D
+
+// push pointer 0
+@3
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// push pointer 1
+@4
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// add
+@SP
+AM=M-1
+D=M
+M=0
+@SP
+A=M-1
+M=M+D
+
+// push this 2
+@2
+D=A
+@THIS
+A=M+D
+D=M
+@SP
 M=M+1
 A=M-1
 M=D
@@ -104,10 +154,13 @@ M=0
 A=M-1
 M=M-D
 
-// push static 8
-@StaticTest.8
+// push that 6
+@6
+D=A
+@THAT
+A=M+D
 D=M
-@SP@SP
+@SP
 M=M+1
 A=M-1
 M=D
